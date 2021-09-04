b53e538abf4e205d5ce404f1cb62a42c36a6704a
unknown <brkysever@gmail.com>
2021-09-04 21:21:49 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <PersonelTable />
+  </div>
+</template>
+
+<script>
+import PersonelTable from "./components/PersonelTable.vue";
+
+export default {
+  name: "App",
+  components: { PersonelTable },
+};
+</script>
+
+<style>
+body {
+  background-color: whitesmoke;
+}
+</style>
---
+++ src/components/PhoneEditable.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p v-if="!isEditMode" @click="isEditMode = true">{{ phone || "-" }}</p>
+    <input v-model="newPhone" @blur="changePhone" v-else autofocus />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhoneEditable",
+  props: ["phone"],
+  data() {
+    return {
+      isEditMode: false,
+      newPhone: this.phone,
+    };
+  },
+  methods: {
+    changePhone() {
+      this.isEditMode = false;
+      this.$emit("phone-changed", this.newPhone);
+    },
+  },
+};
+</script>
+
+<style></style>
